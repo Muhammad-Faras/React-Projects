cff9fde05e58584f7bc040fe3e998bd98fb5172b
Muhammad Faras <noorfaras809@gmail.com>
2024-08-11 16:00:28 +0500

    linkedin (contentSection)

--- Linkedin/src/pages/home/Home.jsx
+++ Linkedin/src/pages/home/Home.jsx
@@ -3,63 +3,148 @@ import Index from '../../components/layout/Index';
 import styles from './Home.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquarePiedPiper } from '@fortawesome/free-brands-svg-icons';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark, faImage, faBriefcase, faNewspaper, faCaretDown, faEllipsis, faXmark, faRetweet, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faCommentDots, } from '@fortawesome/free-regular-svg-icons';
+
 const Home = () => {
     return (
         <Index>
             <div className={`${styles.parentSection}`}>
 
-                <div className={`${styles.profileSection}`}>
-                    <div className={`${styles.profileinfo}`}>
-                        <div className={`${styles.profileheader}`}>
-                            <div className={`${styles.pheaderimg}`}>
-                                <img src="./images/profile_img.jpeg" alt="profile img" />
+                <div className={`${styles.mainSection}`}>
+                    {/* profileSection starts here */}
+                    <div className={`${styles.profileSection}`}>
+                        <div className={`${styles.profileinfo}`}>
+                            <div className={`${styles.profileheader}`}>
+                                <div className={`${styles.pheaderimg}`}>
+                                    <img src="./images/profile_img.jpeg" alt="profile img" />
+                                </div>
+                                <p>Muhammad Faras</p>
+                                <p>Python & Django Developer | HTML | CSS | Tailwind CSS | Git</p>
                             </div>
-                            <p>Muhammad Faras</p>
-                            <p>Python & Django Developer | HTML | CSS | Tailwind CSS | Git</p>
-                        </div>
-                        <div className={`${styles.profileAnalytics}`}>
-                            <div className={`${styles.views}`}>
-                                <p>Profile Viewers</p>
-                                <p>20</p>
+                            <div className={`${styles.profileAnalytics}`}>
+                                <div className={`${styles.views}`}>
+                                    <p>Profile Viewers</p>
+                                    <p>20</p>
+                                </div>
+                                <div className={`${styles.impressions}`}>
+                                    <p>Profile Impressions</p>
+                                    <p>234</p>
+                                </div>
                             </div>
-                            <div className={`${styles.impressions}`}>
-                                <p>Profile Impressions</p>
-                                <p>234</p>
+                            <div className={`${styles.profilePremiumAdd}`} >
+                                <p> Unlock Premium tools & insights</p>
+                                <p><FontAwesomeIcon icon={faSquarePiedPiper} style={{ color: "#FFD43B", marginRight: '3px', }} />Try Premium for PKR0</p>
+                            </div>
+                            <div className={`${styles.profileSaveItem}`}>
+                                <FontAwesomeIcon icon={faBookmark} />
+                                <p>save item</p>
                             </div>
                         </div>
-                        <div className={`${styles.profilePremiumAdd}`} >
-                            <p> Unlock Premium tools & insights</p>
-                            <p><FontAwesomeIcon icon={faSquarePiedPiper} style={{ color: "#FFD43B", marginRight: '3px', }} />Try Premium for PKR0</p>
-                        </div>
-                        <div className={`${styles.profileSaveItem}`}>
-                            <FontAwesomeIcon icon={faBookmark} />
-                            <p>save item</p>
+                        <div className={`${styles.recentinfo}`}>
+                            aaaaaaaaaaasdasdasdasdasda <br />
+                            asdasd
+                            asd
                         </div>
                     </div>
-                    <div className={`${styles.recentinfo}`}>
-                        aaaaaaaaaaasdasdasdasdasda <br />
-                        asdasd
-                        asd
+
+                    {/* -------------------------- */}
+                    {/* contentSection starts here */}
+                    {/* -------------------------- */}
+
+                    <div className={`${styles.contentSection}`}>
+                        <div className={`${styles.postInput}`}>
+                            <div className={`${styles.postEntry}`}>
+                                <img src="./images/profile_img.jpeg" alt="profile img" />
+                                <input type="text" name="" id="" placeholder='Start a post,try writing with AI' />
+                            </div>
+                            <div className={`${styles.postBtns}`}>
+                                <button>
+                                    <FontAwesomeIcon icon={faImage} style={{ color: "#378FE9", fontSize: '1.3rem' }} />
+                                    Media
+                                </button>
+                                <button>
+                                    <FontAwesomeIcon icon={faBriefcase} style={{ color: "#A871EA", fontSize: '1.3rem' }} />
+                                    Job
+                                </button>
+                                <button>
+                                    <FontAwesomeIcon icon={faNewspaper} style={{ color: "#E06847", fontSize: '1.3rem' }} />
+                                    Write Article
+                                </button>
+
+                            </div>
+                        </div>
+                        <div className={`${styles.divider}`}>
+                            <div></div>
+                            <p>Sort by:</p>
+                            <p>Top</p>
+                            <FontAwesomeIcon icon={faCaretDown} style={{ color: "#000000", fontSize: '1rem', cursor: 'pointer' }} />
+                        </div>
+                        <div className={`${styles.postContent}`}>
+                            <div className={`${styles.postCard}`}>
+                                <div className={`${styles.UserContent}`}>
+                                    <div className={`${styles.userData}`}>
+                                        <img src="./images/user.jpeg" alt="post user profile img" />
+                                        <div className={`${styles.userInfo}`}>
+                                            <p>Verified Pakistan Recorder</p>
+                                            <p>610,450 followers</p>
+                                            <p>42m </p>
+                                        </div>
+                                    </div>
+                                    <div className={`${styles.contentActions}`}>
+                                        <FontAwesomeIcon icon={faEllipsis} style={{ color: "#191919", fontSize: '1.5em', cursor: 'pointer' }} />
+                                        <FontAwesomeIcon icon={faXmark} style={{ color: "#191919", fontSize: '1.5em', cursor: 'pointer' }} />
+                                    </div>
+                                </div>
+                                <div className={`${styles.contentTitle}`}>
+                                    <p>Chairman of the Athletics Federation of Pakistan, Major General (Retired) Muhammad Akram Sahi, announced that Arshad Nadeem will be sent on an Umrah pilgrimage along with his
+                                        <span>…see more</span>
+                                    </p>
+                                </div>
+                                <img src="./images/post.jpeg" alt="post pic" className={`${styles.postImage}`} />
+                                <div className={`${styles.contentReactions}`}>
+                                    <button><FontAwesomeIcon icon={faThumbsUp} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Like</span></button>
+                                    <button><FontAwesomeIcon icon={faCommentDots} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Comment</span></button>
+                                    <button><FontAwesomeIcon icon={faRetweet} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Repost</span></button>
+                                    <button><FontAwesomeIcon icon={faPaperPlane} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Send</span></button>
+
+                                </div>
+                            </div>
+                            <div className={`${styles.postCard}`}>
+                                <div className={`${styles.UserContent}`}>
+                                    <div className={`${styles.userData}`}>
+                                        <img src="./images/user.jpeg" alt="post user profile img" />
+                                        <div className={`${styles.userInfo}`}>
+                                            <p>Verified Pakistan Recorder</p>
+                                            <p>610,450 followers</p>
+                                            <p>42m </p>
+                                        </div>
+                                    </div>
+                                    <div className={`${styles.contentActions}`}>
+                                        <FontAwesomeIcon icon={faEllipsis} style={{ color: "#191919", fontSize: '1.5em', cursor: 'pointer' }} />
+                                        <FontAwesomeIcon icon={faXmark} style={{ color: "#191919", fontSize: '1.5em', cursor: 'pointer' }} />
+                                    </div>
+                                </div>
+                                <div className={`${styles.contentTitle}`}>
+                                    <p>Chairman of the Athletics Federation of Pakistan, Major General (Retired) Muhammad Akram Sahi, announced that Arshad Nadeem will be sent on an Umrah pilgrimage along with his
+                                        <span>…see more</span>
+                                    </p>
+                                </div>
+                                <img src="./images/post.jpeg" alt="post pic" className={`${styles.postImage}`} />
+                                <div className={`${styles.contentReactions}`}>
+                                    <button><FontAwesomeIcon icon={faThumbsUp} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Like</span></button>
+                                    <button><FontAwesomeIcon icon={faCommentDots} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Comment</span></button>
+                                    <button><FontAwesomeIcon icon={faRetweet} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Repost</span></button>
+                                    <button><FontAwesomeIcon icon={faPaperPlane} flip="horizontal" style={{ fontSize: '1.5rem' }} /><span>Send</span></button>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                <div className={`${styles.contentSection}`}></div>
+                {/* -------------------------- */}
+                {/* suggestionSection starts here */}
+                {/* -------------------------- */}
                 <div className={`${styles.suggestionSection}`}></div>
             </div>
         </Index >
